8be694956e9162bb1e8e3d7027633113c2893925
Edward Mauricio Suarez <edwardmau98@gmail.com>
2020-06-30 00:52:23 -0500

    fix: error with routes of admin panel. fixed.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'UserController@index');
+Route::get('/', 'HomeController@index');
+Route::get('/administer', 'UserController@index');
 Route::post('users', 'UserController@store')->name('users.store');
 Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
 
