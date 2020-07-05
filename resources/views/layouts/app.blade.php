125269204b62fb3aafbd106906a661367df4fe27
Edward Mauricio Suarez <edwardmau98@gmail.com>
2020-07-05 02:25:10 -0500

    feat: modify nav bar base to add users managment link to admin panel.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,11 @@
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <a class="dropdown-item" href="/admin">
+                                        User Managment
+                                    </a>
+
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
